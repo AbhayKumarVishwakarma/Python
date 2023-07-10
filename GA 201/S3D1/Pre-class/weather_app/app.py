42b19e30a36a01cf896dc0ae2cbeecfe5990bb8c
AbhayKumarVishwakarma <3492akv@gmail.com>
2023-07-10 23:37:01 +0530

    done weather app with TDD and CRUD

--- GA 201/S3D1/Pre-class/weather_app/app.py
+++ GA 201/S3D1/Pre-class/weather_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,14 @@ weather_data = {
 }
 
 
+@app.route('/weather', methods=['GET'])
+def get_all_weather():
+    if len(weather_data) == 0:
+        return jsonify({'message': 'Not found any city in data'}), 404
+
+    return jsonify(weather_data)
+
+
 @app.route('/weather/<string:city>', methods=['GET'])
 def get_weather(city):
     if city in weather_data:
@@ -19,6 +27,49 @@ def get_weather(city):
         return jsonify({'error': 'City not found'}), 404
 
 
+@app.route('/weather', methods=['POST'])
+def add_weather():
+    data = request.get_json()
+
+    if 'city' not in data or 'temperature' not in data or 'weather' not in data:
+        return jsonify({'message': "Error, Invalid data!"}), 400
+
+    city = data['city']
+    temperature = data['temperature']
+    weather = data['weather']
+
+    weather_data[city] = {'temperature': temperature, 'weather': weather}
+
+    return jsonify({'message': f'Weather data for {city} added successfully', 'data': data})
+
+
+@app.route('/weather/<string:city>', methods=['PUT'])
+def update_weather(city):
+    data = request.get_json()
+
+    if city not in weather_data:
+        return jsonify({'message': f'{city} not found!'}), 404
+
+    city_data = weather_data[city]
+
+    if 'temperature' in data:
+        city_data['temperature'] = data['temperature']
+    if 'weather' in data:
+        city_data['weather'] = data['weather']
+
+    return jsonify({'message': f'Weather data for {city} updated successfully',
+                    'data': {'city': city, 'temperature': city_data['temperature'], 'weather': city_data['weather']}})
+
+
+@app.route('/weather/<string:city>', methods=['DELETE'])
+def delete_weather(city):
+    if city not in weather_data:
+        return jsonify({'message': f'{city} not found!'}), 404
+
+    del weather_data[city]
+    return jsonify({'message': f'Weather data for {city} deleted successfully'})
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
--- GA 201/S3D1/Pre-class/weather_app/test_app.py
+++ GA 201/S3D1/Pre-class/weather_app/test_app.py
@@ -11,18 +11,111 @@ def client():
         yield client
 
 
-def test_get_weather_existing_city(client):
-    response = client.get('/weather/New York')
+def test_get_weather_existing_city():
+    client = app.test_client()
+    response = client.get('/weather/Austin')
     data = json.loads(response.data)
 
     assert response.status_code == 200
-    assert data['temperature'] == 20
-    assert data['weather'] == 'Sunny'
+    assert data['temperature'] == 32
+    assert data['weather'] == 'Hot'
 
 
-def test_get_weather_nonexistent_city(client):
+def test_get_weather_nonexistent_city():
+    client = app.test_client()
     response = client.get('/weather/Unknown')
     data = json.loads(response.data)
 
     assert response.status_code == 404
     assert data['error'] == 'City not found'
+
+
+def test_add_weather():
+    data = {
+        'city': 'Chicago',
+        'temperature': 18,
+        'weather': 'Cloudy',
+    }
+    client = app.test_client()
+    response = client.post('/weather', json=data)
+    data = json.loads(response.data)
+
+    assert response.status_code == 200
+    assert data['message'] == 'Weather data for Chicago added successfully'
+    assert data['data'] == {
+        'city': 'Chicago',
+        'temperature': 18,
+        'weather': 'Cloudy',
+    }
+
+
+def test_add_weather_missing_fields():
+    data1 = {
+        'city': 'Houston',
+        'temperature': 28,
+        # missing 'weather' field
+    }
+    data2 = {
+        'city': 'Houston',
+        # missing 'temperature' field
+        'weather': 'Cloudy'
+    }
+
+    client = app.test_client()
+
+    response1 = client.post('/weather', json=data1)
+    data1 = json.loads(response1.data)
+
+    response2 = client.post('/weather', json=data2)
+    data2 = json.loads(response2.data)
+
+    assert response1.status_code == 400
+    assert data1['message'] == 'Error, Invalid data!'
+
+    assert response2.status_code == 400
+    assert data2['message'] == 'Error, Invalid data!'
+
+
+def test_update_weather():
+    data = {'temperature': 34}
+
+    client = app.test_client()
+    response = client.put('/weather/Austin', json=data)
+    data = json.loads(response.data)
+
+    assert response.status_code == 200
+    assert data['message'] == 'Weather data for Austin updated successfully'
+    assert data['data'] == {
+        'city': 'Austin',
+        'temperature': 34,
+        'weather': 'Hot'
+    }
+
+
+def test_update_weather_nonexistent_city():
+    data = {'temperature': 30}
+
+    client = app.test_client()
+    response = client.put('/weather/Unknown city', json=data)
+    data = json.loads(response.data)
+
+    assert response.status_code == 404
+    assert data['message'] == 'Unknown city not found!'
+
+
+def test_delete_weather():
+    client = app.test_client()
+    response = client.delete('/weather/Seattle')
+    data = json.loads(response.data)
+
+    assert response.status_code == 200
+    assert data['message'] == 'Weather data for Seattle deleted successfully'
+
+
+def test_delete_weather_nonexistent_city():
+    client = app.test_client()
+    response = client.delete('/weather/Unknown city')
+    data = json.loads(response.data)
+
+    assert response.status_code == 404
+    assert data['message'] == 'Unknown city not found!'
