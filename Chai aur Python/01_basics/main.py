0a442a6d2fdbb70ae323b3eae4d8e111bcf16055
Abhay Kumar Vishwakarma <vishwakarmaa@alphavima.com>
2025-02-06 10:22:10 +0530

    python basics

---
+++ Chai aur Python/01_basics/main.py
@@ -0,0 +1,31 @@
+one = repr('ab')
+print(one)
+
+two = str('ab')
+print(two)
+
+print('ab')
+
+
+import math
+
+print(math.floor(3.5))
+
+print(math.floor(-3.5))
+
+a = b = c = 10
+print(a, b, c)
+
+a, b, c = 10, 20, 30
+print(a, b, c)
+
+
+num_list = "0123456789"
+
+print(num_list[:])  
+print(num_list[3:])  
+print(num_list[:7])  
+print(num_list[0:7:2])
+print(num_list[0:7:3])
+print(num_list[::2])    
+print(num_list[::-1]) 
